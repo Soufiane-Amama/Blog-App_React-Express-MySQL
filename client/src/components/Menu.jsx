00338fee3b58fb406cad71ca05113ed5fd766be6
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-09 02:02:14 +0100

    first commit

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -47,7 +47,7 @@ const Menu = ({cat}) => {
   return (
     <div className="menu">
       <h1>Other posts you may like</h1>
-      {posts.map((post) => (
+      {posts && posts.map((post) => (
         <div className="post" key={post.id}>
           <img src={`../upload/${post?.img}`} alt="" />
           <h2>{post.title}</h2>
