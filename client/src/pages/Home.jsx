e093a5b845edb6745cd43b938e678d201fb12f27
Soufiane Amama <soufianeamama70@gmail.com>
2023-11-09 01:33:25 +0100

    first commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="posts">
-        {posts.map((post) => (
+        {posts && posts.map((post) => (
           <div className="post" key={post.id}>
             <div className="img">
               <img src={`../upload/${post.img}`} alt="" />
